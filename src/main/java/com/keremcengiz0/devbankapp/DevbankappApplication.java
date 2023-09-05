cc1d50373452959185acbf6bd982168dcae77598
keremcengiz0 <keremcengiz0@hotmail.com>
2023-09-05 12:58:08 +0300

    Initial commit

---
+++ src/main/java/com/keremcengiz0/devbankapp/DevbankappApplication.java
@@ -0,0 +1,13 @@
+package com.keremcengiz0.devbankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevbankappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevbankappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keremcengiz0/devbankapp/DevbankappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keremcengiz0.devbankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevbankappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
